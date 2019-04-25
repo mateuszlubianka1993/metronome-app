020193842fabd6a3dd8c9e331f93be3c99c15aaa
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-04-25 13:26:49 +0200

    add startMetronome, play functions, update onInputChange

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,55 @@
 import React from 'react';
 import Header from './Header';
 import Metronome from './Metronome';
+import click2 from '../sounds/click2.wav';
 
 class App extends React.Component {
-    state = {
-        bpm: 60,
-        isPlaying: false,
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            bpm: 40,
+            isPlaying: false,
+        };
 
+        this.click2 = new Audio(click2);
+    }
+    
     onChangeInput = (e) => {
         // console.log(e.target.value);
-        this.setState({bpm: e.target.value})
+        if (this.state.isPlaying) {
+            clearInterval(this.metronome);
+            this.metronome = setInterval(this.play, (60 / this.state.bpm) * 1000);
+
+            this.setState({
+              bpm: e.target.value
+            });
+          } else {
+            this.setState({ bpm: e.target.value });
+          }
+    };
+    
+    play = () => {
+        this.click2.play()
+    };
+
+    startMetronome = () => {
+        if(this.state.isPlaying) {
+            clearInterval(this.metronome);
+            this.setState({
+                isPlaying: false
+            });
+        } else {
+            this.metronome = setInterval(
+                this.play,
+                (60 / this.state.bpm) * 1000
+              );
+              this.setState(
+                {
+                  isPlaying: true
+                },
+                this.play
+              );
+        }
     };
 
     render() {
@@ -20,6 +59,8 @@ class App extends React.Component {
                 <Metronome 
                     onChangeInput={this.onChangeInput}
                     bpm={this.state.bpm}
+                    startMetronome={this.startMetronome}
+                    isPlaying={this.state.isPlaying}
                 />
             </div>
         );
--- src/components/Metronome.js
+++ src/components/Metronome.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './Metronome.css';
 
 const Metronome = (props) => {
+    let buttonTxt = 'Start';
+    if(props.isPlaying) {
+        buttonTxt = 'Stop';
+    } else {
+        buttonTxt = 'Start';
+    }
     return (
         <div className="metronome-container">
             <div className="metronome-input-box">
                 <div className="bpm-display">{props.bpm}BPM</div>
                 <input type="range" min="40" max="220" onChange={props.onChangeInput}/>
             </div>
-                <button className="positive ui button">Start</button>
+                <button className="positive ui button" onClick={props.startMetronome}>{buttonTxt}</button>
         </div>
     );
 };
