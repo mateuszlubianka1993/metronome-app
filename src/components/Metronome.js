7ff98fa16e1212ca7a3e74de6a65562539577e91
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-04-25 12:24:48 +0200

    add Metronome component and Metronome.css file

--- src/components/Metronome.js
+++ src/components/Metronome.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Metronome.css';
 
 const Metronome = () => {
     return (
         <div className="metronome-container">
-            <div className="bpm-slider">
-                <div>BPM</div>
+            <div className="metronome-input-box">
+                <div className="bpm-display">BPM</div>
                 <input type="range" min="40" max="220" />
             </div>
-                <button>Start</button>
+                <button className="positive ui button">Start</button>
         </div>
     );
 };
