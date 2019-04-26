1286a27059c2987e81f9ed699fdda0a6cfcdea8d
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-04-26 10:59:51 +0200

    Updates

---
+++ precache-manifest.892c83556cd4ca92183007875992acf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "206b995f3854319b90db7b0de4d8ee8f",
+    "url": "/metronome-app/index.html"
+  },
+  {
+    "revision": "cdedb0cdb80a81eea267",
+    "url": "/metronome-app/static/css/main.1d6dd4d0.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/metronome-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "cdedb0cdb80a81eea267",
+    "url": "/metronome-app/static/js/main.003cc930.chunk.js"
+  },
+  {
+    "revision": "d9e778cf73a3feee8699",
+    "url": "/metronome-app/static/js/runtime~main.21ee9e44.js"
+  },
+  {
+    "revision": "beb536f8468fc3ce9adcf8737b0f2560",
+    "url": "/metronome-app/static/media/band-bass-black-and-white-811838.beb536f8.jpg"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/metronome-app/static/media/click2.fe0ded65.wav"
+  }
+]);
