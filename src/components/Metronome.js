ad49e88ccea59d17075bbfb57e54138c4b20ab2f
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-04-26 10:25:02 +0200

    add RWD style

--- src/components/Metronome.js
+++ src/components/Metronome.js
@@ -2,19 +2,22 @@ import React from 'react';
 import './Metronome.css';
 
 const Metronome = (props) => {
+
     let buttonTxt = 'Start';
+    
     if(props.isPlaying) {
         buttonTxt = 'Stop';
     } else {
         buttonTxt = 'Start';
     }
+
     return (
         <div className="metronome-container">
             <div className="metronome-input-box">
-                <div className="bpm-display">{props.bpm}BPM</div>
+                <div className="bpm-display">{props.bpm} BPM</div>
                 <input type="range" min="40" max="220" onChange={props.onChangeInput}/>
             </div>
-                <button className="positive ui button" onClick={props.startMetronome}>{buttonTxt}</button>
+                <button className="positive large ui button" onClick={props.startMetronome}>{buttonTxt}</button>
         </div>
     );
 };
